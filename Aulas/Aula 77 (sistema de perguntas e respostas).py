6933361d391b69533b20208696c588d0fa437aa6
Marques-Otavio <otavio.am14@aluno.ifsc.edu.br>
2025-12-23 23:51:23 -0300

    23/12/2025

---
+++ Aulas/Aula 77 (sistema de perguntas e respostas).py
@@ -0,0 +1,25 @@
+# Exercício - sistema de perguntas e respostas
+
+
+perguntas = [
+    {
+        'Pergunta': 'Quanto é 2+2?',
+        'Opcoes': ['1', '3', '4', '5'],
+        'Resposta': '4',
+    },
+    {
+        'Pergunta': 'Quanto é 5*5?',
+        'Opcoes': ['25', '55', '10', '51'],
+        'Resposta': '25',
+    },
+    {
+        'Pergunta': 'Quanto é 10/2?',
+        'Opcoes': ['4', '5', '2', '1'],
+        'Resposta': '5',
+    },
+]
+
+
+print(perguntas[0]['Pergunta'])
+resposta = input('Opcoes: '+ 0
+
